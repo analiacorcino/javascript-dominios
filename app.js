d35d91d2878a71d14c0d7ee6d84760d50630c8d9
analiacorcino <analia.corcino@gmail.com>
2022-09-23 18:24:41 +0000

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+// el loop tiene 3 parametros inicializacion ; condición ; actualización
+
+for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[i] + adj[i] + noun[i] + ".com");
+}
