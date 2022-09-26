f6f639961293f088420fe16e4af5fb9ee5c06af7
analiacorcino <analia.corcino@gmail.com>
2022-09-26 19:05:08 +0000

    Mi primer commit

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let extensiones = [".com", ".net", ".us", ".io"];
 
 // el loop tiene 3 parametros inicializacion ; condición ; actualización
+// thegreatjogger.com
+//  thegreatracoon.com
+// ourgreatjogger.com
+// ourgreatracoon.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+// ourbigracoon.com
 
-for (let i = 0; i < pronoun.length; i++) {
-    console.log(pronoun[i] + adj[i] + noun[i] + ".com");
+for (let pronombre = 0; pronombre < pronoun.length; pronombre++) {
+    for (let adjetivo = 0; adjetivo < adj.length; adjetivo++) {
+        for (let nombre = 0; nombre < noun.length; nombre++) {
+            for (let dominio = 0; dominio < extensiones.length; dominio++) {
+                console.log(
+                    pronoun[pronombre].concat(
+                        adj[adjetivo],
+                        noun[nombre],
+                        extensiones[dominio]
+                    )
+                );
+            }
+        }
+    }
 }
